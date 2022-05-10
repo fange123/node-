de63ab9069cf5574ef22f2e74c30587c930a79a9
fange123 <2030564240@qq.com>
2022-05-10 10:51:45 +0800

    fix:优化res.send,封装res.cc中间件

--- day09大事件项目/api_server/app.js
+++ day09大事件项目/api_server/app.js
@@ -9,6 +9,19 @@ app.use(cors());
 //* 配置解析表单的中间件,只能解析x-www数据格式
 app.use(express.urlencoded({ extended: false }));
 
+//* 一定要在路由之前封装res.cc函数
+app.use(function (req, res, next) {
+  //success默认值是false，默认处理失败
+  //err可能是错误对象也可能是传入的失败字符串
+  res.cc = function (err, success = false) {
+    res.send({
+      success,
+      message: err instanceof Error ? err.message : err,
+    });
+  };
+  next();
+});
+
 const userRouter = require("./router/user");
 app.use("/api", userRouter);
 
--- day09大事件项目/api_server/router_handler/user.js
+++ day09大事件项目/api_server/router_handler/user.js
@@ -6,19 +6,23 @@ const bcrypt = require("bcryptjs"); //加密
 exports.regUser = (req, res) => {
   const userInfo = req.body;
   if (!userInfo.username || !userInfo.password) {
-    return res.send({ message: "用户名或密码不合法", success: false });
+    // return res.send({ message: "用户名或密码不合法", success: false });
+    return res.cc("用户名或密码不合法");
   }
 
   //* 定义sql语句，判断用户名是否被占用
   const sqlStr = "select * from ev_users where username = ?";
   db.query(sqlStr, userInfo.username, (err, result) => {
-    if (err) return res.send({ message: "查询失败", success: false });
+    // if (err) return res.send({ message: "查询失败", success: false });
+    if (err) return res.cc(err);
 
     if (result.length > 0) {
-      return res.send({
-        message: "用户名被占用，请更换用户名",
-        success: false,
-      });
+      // return res.send({
+      //   message: "用户名被占用，请更换用户名",
+      //   success: false,
+      // });
+
+      return res.cc("用户名被占用，请更换用户名");
     }
     //* 对密码进行加密处理(明文密码，随机盐的长度)
     userInfo.password = bcrypt.hashSync(userInfo.password, 10);
@@ -30,12 +34,15 @@ exports.regUser = (req, res) => {
       sqlStr,
       { username: userInfo.username, password: userInfo.password },
       (err, result) => {
-        if (err) return res.send({ success: false, message: err.message });
+        // if (err) return res.send({ success: false, message: err.message });
+        if (err) return res.cc(err);
 
         if (result.affectedRows !== 1)
-          return res.send({ success: false, message: "注册失败，清稍后再试" });
+          // return res.send({ success: false, message: "注册失败，清稍后再试" });
+          return res.cc("注册失败，清稍后再试");
 
-        res.send({ message: "注册成功", success: true });
+        // res.send({ message: "注册成功", success: true });
+        res.cc("注册成功", true);
       }
     );
   });
